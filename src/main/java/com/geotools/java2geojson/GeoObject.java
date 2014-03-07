0617ca820e1671c9ba8fb130a98ff408ab2df013
skalex <alecio88@gmail.com>
2014-03-07 14:30:36 -0300

    First commit

---
+++ src/main/java/com/geotools/java2geojson/GeoObject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geotools.java2geojson;
+
+/**
+ *
+ * @author sKaleX
+ */
+public class GeoObject {
+    
+    protected String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
